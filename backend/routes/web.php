bab2855f8169a3cc9bf8eac0c00b8ac6b68f1630
Surbhi <surbhikandhway12@gmail.com>
2026-05-11 00:55:00 +0530

    Updated web route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'status' => 'Laravel app working on Railway 🚀'
-    ]);
+    return view('welcome');
 });
