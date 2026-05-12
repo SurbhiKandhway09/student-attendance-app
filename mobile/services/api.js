b9deb84282b0e7292146cc62d18c64df86bc2728
Surbhi <surbhikandhway12@gmail.com>
2026-05-13 02:27:14 +0530

    Final attendance app deployment

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.16:8080/api"
+  baseURL: "https://student-attendance-app-production.up.railway.app/api"
 });
 
 export default API;
