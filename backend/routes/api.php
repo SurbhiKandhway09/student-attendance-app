a0dac9357da21eea3ef9a56326512c0ebebfa537
Surbhi <surbhikandhway12@gmail.com>
2026-05-13 01:37:39 +0530

    Add attendance API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,20 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\Api\AttendanceController;
 
-Route::get('/students', [
-    AttendanceController::class,
-    'students'
-]);
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Attendance API Working'
+    ]);
+});
 
-Route::post('/attendance', [
-    AttendanceController::class,
-    'markAttendance'
-]);
-
-Route::get('/history', [
-    AttendanceController::class,
-    'history'
-]);
+Route::get('/attendance', [AttendanceController::class, 'index']);
+Route::post('/attendance', [AttendanceController::class, 'store']);
