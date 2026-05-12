bc1f71da73aba66a994ec263c7a68e74057b1d05
Surbhi <surbhikandhway12@gmail.com>
2026-05-13 01:53:18 +0530

    Add database seeder

--- backend/database/seeders/StudentSeeder.php
+++ backend/database/seeders/StudentSeeder.php
@@ -5,26 +5,13 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class StudentSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      */
     public function run(): void
     {
-        \App\Models\Student::create([
-        'name' => 'Rahul',
-        'roll_number' => '101'
-    ]);
-
-    \App\Models\Student::create([
-        'name' => 'Aman',
-        'roll_number' => '102'
-    ]);
-
-    \App\Models\Student::create([
-        'name' => 'Priya',
-        'roll_number' => '103'
-    ]);
+        $this->call(StudentSeeder::class);
     }
 }
